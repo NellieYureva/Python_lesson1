aa4393637dcdb3e3df1be240c143de383ab990e1
Yureva Nelli <jurjevanellie@mail.ru>
2024-02-04 12:06:37 +0300

    Python_lesson1

---
+++ Tast1.py
@@ -0,0 +1,11 @@
+# Введите два числа и определите большее из них
+
+num1 = 80
+num2 = 52
+
+if num1 == num2:
+    print (f'num1={num1} = num2={num2}')
+elif num1 > num2:
+    print (f'num1={num1} > num2={num2}')
+else:
+    print (f'num1={num1} < num2={num2}')
